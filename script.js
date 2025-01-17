800695827e58010dcd476acc18deec79889b0a97
Emma Grace <emmalaslo@gmail.com>
2025-01-17 00:02:51 -0500

    Added the contact page and success page and updated the JS file

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("Hello World");
+// Form Submission Logic COPIED FROM CANVAS PAGE
+const form = document.querySelector("#form");
+const submitButton = document.querySelector("#submit");
+
+if (form) {
+  // Ensure this runs only on pages with the form
+  form.addEventListener("submit", (e) => {
+    submitButton.disabled = true; // Disable the submit button
+    e.preventDefault(); // Prevent form default behavior
+    window.location.href = window.location.origin + "/success.html"; // Redirect to success page
+  });
+}
+
+// Success Page Redirection Logic
+if (window.location.pathname.includes("success.html")) {
+  // Ensure this runs only on the success page
+  setTimeout(() => {
+    window.location.href = window.location.origin; // Redirect to home page
+  }, 5000);
+}
